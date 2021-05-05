2f60799b12349e2f6cb61150474d480c24cb477e
Joe Fleming <github@joefleming.net>
2021-05-05 10:58:57 -0700

    fix: handle null shared and lambdaSrcDirs values

--- src/getPaths.js
+++ src/getPaths.js
@@ -5,5 +5,15 @@ module.exports = async function getPaths() {
   const { inv: inventory } = await getInventory({ cwd: path.join(__dirname, '..') });
 
   // NOTE: no need to worry about inventory.macros, the are not deployed
-  return [path.resolve(inventory.shared.src), ...inventory.lambdaSrcDirs];
+  const sharedPath = inventory.shared != null ? [path.resolve(inventory.shared.src)] : [];
+
+  if (inventory.lambdaSrcDirs == null) {
+    if (sharedPath.length === 0) {
+      throw new Error('No lambdas found in project');
+    }
+
+    return sharedPath;
+  }
+
+  return sharedPath.concat(inventory.lambdaSrcDirs);
 };
